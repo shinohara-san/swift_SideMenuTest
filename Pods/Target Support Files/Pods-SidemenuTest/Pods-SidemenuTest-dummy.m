946b2a1b55a8512954ee4bcbf793bd3ed7f6b076
shinohara-san <someiyoshino0809@gmail.com>
2020-07-08 15:05:18 +0900

    first

---
+++ Pods/Target Support Files/Pods-SidemenuTest/Pods-SidemenuTest-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SidemenuTest : NSObject
+@end
+@implementation PodsDummy_Pods_SidemenuTest
+@end
--- SidemenuTest/ViewController.swift
+++ SidemenuTest/ViewController.swift
@@ -5,16 +5,52 @@
 //  Created by Yuki Shinohara on 2020/07/08.
 //  Copyright © 2020 Yuki Shinohara. All rights reserved.
 //
-
 import UIKit
+import SideMenu
 
 class ViewController: UIViewController {
+    
+    var menu: SideMenuNavigationController?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        menu = SideMenuNavigationController(rootViewController: ListViewController()) //メニューを配置
+        menu?.leftSide = true //左側に
+        menu?.setNavigationBarHidden(true, animated: false) //ナビゲーションバーを隠す
+        
+        SideMenuManager.default.leftMenuNavigationController = menu //スワイプするメニューの設定(ないとスワイプできん)
+        SideMenuManager.default.addPanGestureToPresent(toView: self.view) //開いてなくてもスワイプしたら開く設定
+        
     }
-
-
+    
+    @IBAction func didTapButton(){
+        present(menu!, animated: true)
+    }
 }
 
+class ListViewController: UITableViewController {
+    let array = ["first", "second", "third", "first", "second", "third", "first", "second", "third"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.backgroundColor = .blue
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = array[indexPath.row]
+        cell.textLabel?.textColor = .white
+        cell.backgroundColor = .blue
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        //do something
+    }
+}
